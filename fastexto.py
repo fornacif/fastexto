502fceb94e2b57ff647a50d8be9426a1934fd35a
Francois Fornaciari <francois.fornaciari@gmail.com>
2013-01-06 10:28:17 +0100

    Handle authentication process

--- fastexto.py
+++ fastexto.py
@@ -5,6 +5,7 @@ import jinja2
 import os
 
 from google.appengine.ext import db
+from google.appengine.api import users
 from sfrswallow import SMSSender
 
 jinja_environment = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
@@ -17,24 +18,38 @@ class SMS(db.Model):
 	
 class Main(webapp2.RequestHandler):
 	def get(self):
-		template_values = {}
-		template = jinja_environment.get_template("index.html")
-		self.response.out.write(template.render(template_values))
+		user = authenticate(self)
+		if (user):
+			template_values = {
+				"nickname": user.nickname(),
+				"logoutURL": users.create_logout_url("/")
+			}
+			template = jinja_environment.get_template("index.html")
+			self.response.out.write(template.render(template_values))
 		
 class Send(webapp2.RequestHandler):
 	def post(self):
-		sms = SMS()
-		sms.username = self.request.get("username")
-		sms.password = self.request.get("password")
-		sms.phonenumber = self.request.get("phonenumber")
-		sms.message = self.request.get("message")
-		self.send(sms)
-		sms.put()
-		self.redirect("/")
-	def send(self, sms):
-		logging.info("Sending message to %s", str(sms.phonenumber))
-		sender = SMSSender(sms.username, sms.password)
-		sender.sendsms(sms.phonenumber, sms.message)
+		user = authenticate(self)
+		if (user):
+			sms = SMS()
+			sms.username = self.request.get("username")
+			sms.password = self.request.get("password")
+			sms.phonenumber = self.request.get("phonenumber")
+			sms.message = self.request.get("message")
+			
+			logging.info("Sending message to %s", str(sms.phonenumber))
+			sender = SMSSender(sms.username, sms.password)
+			sender.sendsms(sms.phonenumber, sms.message)
+			
+			sms.put()
+			self.redirect("/")
+		
+def authenticate(self):
+	user = users.get_current_user()
+	if user is None:
+		self.redirect(users.create_login_url(self.request.uri))
+	else:
+		return user
 
 app = webapp2.WSGIApplication([
 	("/", Main),
