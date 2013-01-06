3d3f718bf5584ae13cabd8f495875ac737138acd
Francois Fornaciari <francois.fornaciari@gmail.com>
2013-01-06 09:58:34 +0100

    Add Jinja2 Templates

--- fastexto.py
+++ fastexto.py
@@ -1,87 +1,42 @@
 ﻿import webapp2
 import logging
 import md5
+import jinja2
+import os
 
 from google.appengine.ext import db
 from sfrswallow import SMSSender
 
+jinja_environment = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
+
 class SMS(db.Model):
 	username = db.StringProperty(multiline=False)
-	password = ''
+	password = ""
 	phonenumber = db.StringProperty(multiline=False)
 	message = db.StringProperty(multiline=True)
 	
 class Main(webapp2.RequestHandler):
 	def get(self):
-		self.response.headers['Content-Type'] = 'text/html'
-		self.response.out.write("""
-			<html>
-				<head>
-					<meta charset="utf-8">
-					<title>SMS Sender</title>
-					<meta name="viewport" content="width=device-width, initial-scale=1.0">
-					<meta name="description" content="SMS Sender">
-					<meta name="author" content="François Fornaciari">
-					<link href="./bootstrap/css/bootstrap.css" rel="stylesheet">
-					<style>
-						body {
-							padding-top: 20px;
-						}
-					</style>
-					<link href="./bootstrap/css/bootstrap-responsive.css" rel="stylesheet">
-				</head>
-				<body>
-					<div class="container">
-						<form action="/send" method="post" accept-charset="UTF-8">
-							<fieldset>
-								<legend>SMS Sender</legend>
-								<label>Username</label>
-								<input type="tel" name="username" placeholder="0102030405" required maxlength="10"/>
-								<label>Password</label>
-								<input type="password" name="password" required/>
-								<label>Phone Number</label>
-								<input type="tel" name="phonenumber" placeholder="0102030405" required maxlength="10"/>
-								<label>Message</label>
-								<textarea id="messageTA" name="message" rows="5"></textarea>
-								<label id="messageLengthLabel">0 SMS</label>
-								<div class="form-actions">
-									<button type="submit" class="btn btn-primary">Send SMS</button>
-								</div>
-							</fieldset>
-						</form>
-					</div>
-					<script src="http://code.jquery.com/jquery-latest.js"></script>
-					<script type="text/javascript">
-						$(document).ready(function(){
-							$("#messageTA").keyup(function() {
-								var currentString = $("#messageTA").val();
-								var numMessages = Math.floor(currentString.length / 140);
-								if ((currentString.length % 140) > 0) {
-									numMessages++;
-								}
-								$("#messageLengthLabel").html(numMessages + " SMS (" + currentString.length + " caractères)");
-							});
-						});
-					</script>
-				</body>
-			</html>""")
+		template_values = {}
+		template = jinja_environment.get_template("index.html")
+		self.response.out.write(template.render(template_values))
 		
 class Send(webapp2.RequestHandler):
 	def post(self):
 		sms = SMS()
-		sms.username = self.request.get('username')
-		sms.password = self.request.get('password')
-		sms.phonenumber = self.request.get('phonenumber')
-		sms.message = self.request.get('message')
+		sms.username = self.request.get("username")
+		sms.password = self.request.get("password")
+		sms.phonenumber = self.request.get("phonenumber")
+		sms.message = self.request.get("message")
 		self.send(sms)
 		sms.put()
-		self.redirect('/')
+		self.redirect("/")
 	def send(self, sms):
 		logging.info("Sending message to %s", str(sms.phonenumber))
 		sender = SMSSender(sms.username, sms.password)
 		sender.sendsms(sms.phonenumber, sms.message)
 
 app = webapp2.WSGIApplication([
-	('/', Main),
-	('/send', Send)
+	("/", Main),
+	("/send", Send)
 ], debug=True)
--- sfrswallow.py
+++ sfrswallow.py
@@ -1,31 +1,8 @@
-#! /usr/bin/env python
-#   -*- encoding: utf-8 -*-
-#
-#   SFR Swallow - send SMS through the SFR web interface
-#   Version 0.4
-#
-#   Copyright (c) 2010, Émile Decorsière, Florian Birée
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 3 of the License, or (at
-# your option) any later version.
-#
-# This program is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-
 import urllib
 import mechanize
 import logging
 
-# urls
+# URLs
 SFR_LOGIN_URL = 'https://www.sfr.fr/mon-espace-client'
 SFR_COMPOSE_URL = 'http://www.sfr.fr/xmscomposer/index.html?todo=compose'
 SFR_CONFIRM_URL = 'http://www.sfr.fr/xmscomposer/mc/envoyer-texto-mms/send.html'
